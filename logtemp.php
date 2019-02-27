5fee0c17cb4215510627e40e2286b61bcd55ef4e
hwiggerm <hwiggerm@gmail.com>
2019-02-27 10:39:08 +0100

    Update logtemp.php

--- logtemp.php
+++ logtemp.php
@@ -12,22 +12,26 @@
   $password = "test623";
   $dbname = "testdb";
 
-  echo "Naam :" ;
-
   try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
+    
+    if(!empty($_POST['sdevice']) && !empty($_POST['temp']) && !empty($_POST['humidity']) )
+    {
+      $device = $_POST['device'];
+      $temp = $_POST['temp'];
+      $humidity = $_POST['humidity'];
 
-    $sql = "insert into temp_log(device,temp,humidity) VALUES ($device, $temp, $humidity)";
+	    $sql = "insert into temp_log(device,temp,humidity) VALUES ($device, $temp, $humidity)";
 
-    if ($conn->query($sql) == TRUE) {
-    echo "New record created successfully";
-    } else {
-    echo "......Error: " . $sql . "<br>" . $conn->error;
+      if ($conn->query($sql) == TRUE) {
+      echo "New record created successfully";
+      } else {
+       echo "......Error: " . $sql . "<br>" . $conn->error;
+	   }
   }
-
 
 
   } catch(PDOException $e) {
